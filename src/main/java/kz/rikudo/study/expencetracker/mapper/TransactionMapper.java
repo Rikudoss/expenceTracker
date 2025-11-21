f98307cf91cbbd1ea72c032555b3f7217aae8e9c
yerkin <yerkinkurmanali@gmail.com>
2025-11-21 11:33:45 +0500

    v2

--- src/main/java/kz/rikudo/study/expencetracker/mapper/TransactionMapper.java
+++ src/main/java/kz/rikudo/study/expencetracker/mapper/TransactionMapper.java
@@ -4,7 +4,7 @@ import kz.rikudo.study.expencetracker.dto.TransactionRequestDTO;
 import kz.rikudo.study.expencetracker.dto.TransactionResponseDTO;
 import kz.rikudo.study.expencetracker.entity.Transaction;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
